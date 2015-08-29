d730451990bf345510249735cf31420dcbe35159
Philip Diehl <pdd845@hotmail.com>
2015-08-28 22:36:33 -0400

    This commit includes: -Naming convention update for primitive types.

--- Solution/PhoenixEngine/Source/Utility/Primitives.h
+++ Solution/PhoenixEngine/Source/Utility/Primitives.h
@@ -5,18 +5,18 @@ namespace Phoenix
 {
 	using FChar = char;
 
-	using SI8 = signed char;
-	using SI16 = signed short;
-	using SI32 = signed int;
-	using SI64 = signed long long;
+	using Int8 = signed char;
+	using Int16 = signed short;
+	using Int32 = signed int;
+	using Int64 = signed long long;
 
-	using UI8 = unsigned char;
-	using UI16 = unsigned short;
-	using UI32 = unsigned int;
-	using UI64 = unsigned long long;
+	using UInt8 = unsigned char;
+	using UInt16 = unsigned short;
+	using UInt32 = unsigned int;
+	using UInt64 = unsigned long long;
 
-	using F32 = float;
-	using F64 = double;
+	using Float32 = float;
+	using Float64 = double;
 
 	using SizeT = size_t;
 	using NullptrT = std::nullptr_t;
