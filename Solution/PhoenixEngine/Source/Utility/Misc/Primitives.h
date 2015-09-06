f0c93bb14d1dea1fdd97985ac5b8881114cbfa35
Mitchell Andrews <rastaninja77@gmail.com>
2015-09-06 02:51:23 -0400

    Added namespace to size_t for build server

--- Solution/PhoenixEngine/Source/Utility/Misc/Primitives.h
+++ Solution/PhoenixEngine/Source/Utility/Misc/Primitives.h
@@ -18,7 +18,7 @@ namespace Phoenix
 	typedef float Float32;
 	typedef double Float64;
 
-	typedef size_t SizeT;
+	typedef std::size_t SizeT;
 	typedef std::nullptr_t NullptrT;
 
 	enum class EInitParam
