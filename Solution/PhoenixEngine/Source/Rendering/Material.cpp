9ffa99162d9c53c0bff3763fffcd8f7fa548d29c
Philip Diehl <pdd845@hotmail.com>
2015-09-19 20:33:39 -0400

    This commit includes: -Adding Material.h / .cpp

---
+++ Solution/PhoenixEngine/Source/Rendering/Material.cpp
@@ -0,0 +1,174 @@
+#include "Rendering/Material.h"
+
+#include "Utility/Debug/Assert.h"
+
+using namespace Phoenix;
+
+bool EMaterialType::IsValid(const EMaterialType::Type MaterialType)
+{
+	const bool IsEnumValid = MaterialType < EMaterialType::Count;
+	return IsEnumValid;
+}
+
+FVector2D FMaterialBase::ColourRange = FVector2D(0.f, 1.f);
+
+FMaterialBase FMaterialBase::Create(
+	const EMaterialType::Value Type,
+	const FVector4D& Colour)
+{
+	F_Assert(EMaterialType::IsValid(Type), "Material type is invalid.");
+	// #FIXME: Validate colour
+
+	FMaterialBase MaterialBase;
+
+	MaterialBase.Type = Type;
+	MaterialBase.Colour = Colour;
+
+	return MaterialBase;
+}
+
+const FVector2D FAshikMaterial::AnisotropicRange = FVector2D(1.f, 10000.f);
+
+FAshikMaterial FAshikMaterial::Create(
+	const Float32 AnisotropicU,
+	const Float32 AnisotropicV)
+{
+	// #FIXME: Validate via clamp ((val, min, max) == val) operation.
+	FAshikMaterial Ashik;
+
+	Ashik.AnisotropicU = AnisotropicU;
+	Ashik.AnisotropicV = AnisotropicV;
+
+	return Ashik;
+}
+
+const FVector2D FBlinnMaterial::SpecularExpRange = FVector2D(1.f, 1024.f);
+
+FBlinnMaterial FBlinnMaterial::Create(
+	const Float32 SpecularExp)
+{
+	// #FIXME: Validate via clamp operation.
+	FBlinnMaterial Blinn;
+
+	Blinn.SpecularExp = SpecularExp;
+
+	return Blinn;
+}
+
+const FVector2D FCookMaterial::RoughnessRange = FVector2D(0.00001f, 1.f);
+const FVector2D FCookMaterial::RefANIRange = FVector2D(1.f, 5.f);
+
+FCookMaterial FCookMaterial::Create(
+	const Float32 Roughness,
+	const Float32 RefANI)
+{
+	// #FIXME: Validate via clamp operations.
+	FCookMaterial Cook;
+
+	Cook.Roughness = Roughness;
+	Cook.RefANI = RefANI;
+
+	return Cook;
+}
+
+const FVector2D FStraussMaterial::SmoothnessRange = FVector2D(0.f, 0.99999f);
+const FVector2D FStraussMaterial::MetalnessRange = FVector2D(0.f, 1.f);
+const FVector2D FStraussMaterial::TransparencyRange = FVector2D(0.f, 1.f);
+
+FStraussMaterial FStraussMaterial::Create(
+	const Float32 Smoothness,
+	const Float32 Metalness,
+	const Float32 Transparency)
+{
+	// #FIXME: Validate via clamp operations.
+	FStraussMaterial Strauss;
+
+	Strauss.Smoothness = Smoothness;
+	Strauss.Metalness = Metalness;
+	Strauss.Transparency = Transparency;
+
+	return Strauss;
+}
+
+const FVector2D FWardMaterial::RoughnessRange = FVector2D(0.f, 1.f);
+
+FWardMaterial FWardMaterial::Create(
+	const Float32 Roughness)
+{
+	// #FIXME: Validate via clamp operation.
+	FWardMaterial Ward;
+
+	Ward.Roughness = Roughness;
+
+	return Ward;
+}
+
+FMaterial FMaterial::CreateDefault(const FVector4D& Colour)
+{
+	const FMaterial Material = CreateBlinn(Colour, FBlinnMaterial::SpecularExpRange.x);
+	return Material;
+}
+
+FMaterial FMaterial::CreateAshik(
+	const FVector4D& Colour,
+	const Float32 AnisotropicU,
+	const Float32 AnisotropicV)
+{
+	FMaterial Material;
+
+	Material.Base = FMaterialBase::Create(EMaterialType::Ashik, Colour);
+	Material.Ashik = FAshikMaterial::Create(AnisotropicU, AnisotropicV);
+
+	return Material;
+}
+
+FMaterial FMaterial::CreateBlinn(
+	const FVector4D& Colour,
+	const Float32 SpecularExp)
+{
+	FMaterial Material;
+
+	Material.Base = FMaterialBase::Create(EMaterialType::Blinn, Colour);
+	Material.Blinn = FBlinnMaterial::Create(SpecularExp);
+
+	return Material;
+}
+
+FMaterial FMaterial::CreateCook(
+	const FVector4D& Colour,
+	const Float32 Roughness,
+	const Float32 RefANI)
+{
+	FMaterial Material;
+
+	Material.Base = FMaterialBase::Create(EMaterialType::Cook, Colour);
+	Material.Cook = FCookMaterial::Create(Roughness, RefANI);
+
+	return Material;
+}
+
+FMaterial FMaterial::CreateStrauss(
+	const FVector4D& Colour,
+	const Float32 Smoothness,
+	const Float32 Metalness,
+	const Float32 Transparency)
+{
+	FMaterial Material;
+
+	Material.Base = FMaterialBase::Create(EMaterialType::Strauss, Colour);
+	Material.Strauss = FStraussMaterial::Create(Smoothness, Metalness, Transparency);
+
+	return Material;
+}
+
+FMaterial FMaterial::CreateWard(
+	const FVector4D& Colour,
+	const Float32 Roughness)
+{
+	FMaterial Material;
+
+	Material.Base = FMaterialBase::Create(EMaterialType::Ward, Colour);
+	Material.Ward = FWardMaterial::Create(Roughness);
+	
+	return Material;
+}
---
+++ Solution/PhoenixEngine/Source/Rendering/Material.h
@@ -0,0 +1,143 @@
+#ifndef PHOENIX_MATERIAL_H
+#define PHOENIX_MATERIAL_H
+
+#include "Utility/Containers/Array.h"
+#include "Utility/Misc/Primitives.h"
+#include "Math/Vector2D.h"
+#include "Math/Vector4D.h"
+
+namespace Phoenix
+{
+	namespace EMaterialType
+	{
+		using Type = UInt8;
+
+		enum Value : Type
+		{
+			Blinn,
+			Cook,
+			Strauss,
+			Ward,
+			Ashik,
+			Count
+		};
+
+		static bool IsValid(const EMaterialType::Type MaterialType);
+	}
+		
+	struct FMaterialBase
+	{
+		static FVector2D ColourRange;
+
+		EMaterialType::Value Type;
+		FVector4D Colour;
+
+		static FMaterialBase Create(
+			const EMaterialType::Value Type,
+			const FVector4D& Colour);
+	};
+
+	struct FAshikMaterial
+	{
+		static const FVector2D AnisotropicRange;
+
+		Float32 AnisotropicU;
+		Float32 AnisotropicV;
+
+		static FAshikMaterial Create(
+			const Float32 AnisotropicU,
+			const Float32 AnisotropicV);
+	};
+
+	struct FBlinnMaterial
+	{
+		static const FVector2D SpecularExpRange;
+
+		Float32 SpecularExp;
+
+		static FBlinnMaterial Create(
+			const Float32 SpecularExp);
+	};
+
+	struct FCookMaterial
+	{
+		static const FVector2D RoughnessRange;
+		static const FVector2D RefANIRange;
+
+		Float32 Roughness;
+		Float32	RefANI;
+
+		static FCookMaterial Create(
+			const Float32 Roughness,
+			const Float32 RefANI);
+	};
+
+	struct FStraussMaterial
+	{
+		static const FVector2D SmoothnessRange;
+		static const FVector2D MetalnessRange;
+		static const FVector2D TransparencyRange;
+
+		Float32 Smoothness;
+		Float32 Metalness;
+		Float32 Transparency;
+
+		static FStraussMaterial Create(
+			const Float32 Smoothness, 
+			const Float32 Metalness, 
+			const Float32 Transparency);
+	};
+
+	struct FWardMaterial
+	{
+		static const FVector2D RoughnessRange;
+
+		Float32 Roughness;
+
+		static FWardMaterial Create(
+			const Float32 Roughness);
+	};
+
+	struct FMaterial
+	{
+		FMaterialBase Base;
+		union
+		{
+			FAshikMaterial Ashik;
+			FBlinnMaterial Blinn;
+			FCookMaterial Cook;
+			FStraussMaterial Strauss;
+			FWardMaterial Ward;
+		};
+
+		static FMaterial CreateDefault(const FVector4D& Colour);
+		
+		static FMaterial CreateAshik(
+			const FVector4D& Colour, 
+			const Float32 AnisotropicU, 
+			const Float32 AnisotropicV);
+		
+		static FMaterial CreateBlinn(
+			const FVector4D& Colour, 
+			const Float32 SpecularExp);
+		
+		static FMaterial CreateCook(
+			const FVector4D& Colour, 
+			const Float32 Roughness,
+			const Float32 RefANI);
+		
+		static FMaterial CreateStrauss(
+			const FVector4D& Colour, 
+			const Float32 Smoothness, 
+			const Float32 Metalness,
+			const Float32 Transparency);
+
+		static FMaterial CreateWard(
+			const FVector4D& Colour,
+			const Float32 Roughness);
+	};
+}
+
+
+
+#endif
