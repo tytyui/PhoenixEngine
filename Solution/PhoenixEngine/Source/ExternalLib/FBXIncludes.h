7089a45eb79e23487ff2d5f81a03ddf7ab4bf769
Mitchell Andrews <rastaninja77@gmail.com>
2015-09-26 13:23:29 -0400

    FBX Include fix

--- Solution/PhoenixEngine/Source/ExternalLib/FBXIncludes.h
+++ Solution/PhoenixEngine/Source/ExternalLib/FBXIncludes.h
@@ -2,8 +2,8 @@
 #define PHOENIX_FBX_INCLUDES_H
 
 //#FIXME: These files need to be able to be included:
-// #include <fbxsdk/core/base/fbxarray.h>
+#include <fbxsdk/core/base/fbxarray.h>
 // #FIXME: Uncomment the below line when the above issue is fixed.
-//#include "FBX/fbxsdk.h"
+#include "FBX/fbxsdk.h"
 
 #endif
--- Solution/Premake.lua
+++ Solution/Premake.lua
@@ -2,9 +2,9 @@ workspace "PhoenixEngine"
 	configurations { "Debug", "Release" }
 	platforms{ "x32", "x64" }
 	
-	includedirs { "PhoenixEngine/Source", "Libraries/Include" }
+	includedirs { "PhoenixEngine/Source", "Libraries/Include", "Libraries/Include/FBX/" }
 	libdirs {
-		"Libraries/Lib/%{cfg.buildcfg}.%{cfg.platform}/**",		
+		"Libraries/Lib/%{cfg.buildcfg}.%{cfg.platform}/**",
 		"/usr/local/lib",		
 		os.findlib("glfw3"),		
 		os.findlib("glew")
@@ -13,7 +13,7 @@ workspace "PhoenixEngine"
 	links { "glfw3", "irrklang" }
 	
 	configuration "vs2015"
-		links { "glew32s", "opengl32" }
+		links { "glew32s", "opengl32", "libfbxsdk-md" }
 	
 	configuration "gmake"
 		links { "glew", "opengl" }
@@ -23,12 +23,12 @@ workspace "PhoenixEngine"
 		buildoptions { "-F ./Libraries/Lib/%{cfg.buildcfg}.%{cfg.platform}/IrrKlang -L ./Libraries/Lib/%{cfg.buildcfg}.%{cfg.platform}/IrrKlang" }
 		linkoptions { "-F ./Libraries/Lib/%{cfg.buildcfg}.%{cfg.platform}/IrrKlang -L ./Libraries/Lib/%{cfg.buildcfg}.%{cfg.platform}/IrrKlang"  }
 	
-	configuration { "x32" }
-		debugdir "Libraries/Dll/x32/"
-	
-	configuration { "x64" }
+	filter "configurations:x64"
 		debugdir "Libraries/Dll/x64/"
 
+	filter "configurations:x32"
+		debugdir "Libraries/Dll/x32/"
+	
 project "PhoenixEngine"
 	kind "StaticLib"
 	language "C++"
